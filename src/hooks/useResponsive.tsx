48a48e2867f8574e3b9ea2a684fbed1185228e33
Elitsa Vrancheva <Elitsa@funda.nl>
2024-03-23 19:10:58 +0100

    Fix TS error

--- src/hooks/useResponsive.tsx
+++ src/hooks/useResponsive.tsx
@@ -1,33 +1,38 @@
-// @mui
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { Breakpoint } from "@mui/material/styles";
 
 // ----------------------------------------------------------------------
 
-export default function useResponsive(query, key, start, end) {
+export default function useResponsive(
+  query: "up" | "down" | "between" | "only",
+  key: Breakpoint,
+  start?: Breakpoint,
+  end?: Breakpoint
+) {
   const theme = useTheme();
 
   const mediaUp = useMediaQuery(theme.breakpoints.up(key));
 
   const mediaDown = useMediaQuery(theme.breakpoints.down(key));
 
-  const mediaBetween = useMediaQuery(theme.breakpoints.between(start, end));
+  const mediaBetween = useMediaQuery(theme.breakpoints.between(start!, end!));
 
   const mediaOnly = useMediaQuery(theme.breakpoints.only(key));
 
-  if (query === 'up') {
+  if (query === "up") {
     return mediaUp;
   }
 
-  if (query === 'down') {
+  if (query === "down") {
     return mediaDown;
   }
 
-  if (query === 'between') {
+  if (query === "between") {
     return mediaBetween;
   }
 
-  if (query === 'only') {
+  if (query === "only") {
     return mediaOnly;
   }
   return null;
