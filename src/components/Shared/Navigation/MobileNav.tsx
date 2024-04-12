1196af0a61f3365a7f7e623cf657641983fcacc9
Elitsa Vrancheva <Elitsa@funda.nl>
2024-04-12 13:13:14 +0200

    WIP: Mobile navigation

---
+++ src/components/Shared/Navigation/MobileNav.tsx
@@ -0,0 +1,72 @@
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import { useState } from "react";
+import { List as ListIcon } from "phosphor-react";
+import useLocalStorage from "../../../hooks/useLocalStore";
+import { ChatItem } from "../../Chats/types";
+import { Link } from "react-router-dom";
+import Logo from "../../../assets/Images/logo.ico";
+import { useTheme } from "@mui/material/styles";
+
+export default function MobileNav() {
+  const [open, setOpen] = useState(false);
+  const [chatItems] = useLocalStorage<ChatItem[]>("ChatItems", []);
+  const theme = useTheme();
+
+  const toggleDrawer = () => {
+    console.log(chatItems);
+    setOpen(!open);
+  };
+
+  return (
+    <Box>
+      <AppBar sx={{ position: "absolute" }}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={toggleDrawer}
+            sx={{ mr: 2 }}
+          >
+            <ListIcon size={32} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={open} onClose={toggleDrawer}>
+        <Link to="/chat" style={{ textDecoration: "none" }}>
+          <Box
+            sx={{
+              height: 48,
+              width: 48,
+              borderRadius: 1.5,
+              backgroundColor: theme.palette.primary.main,
+            }}
+            p={1}
+          >
+            <img src={Logo} alt="Tawk" height={48} width={48} />
+          </Box>
+        </Link>
+        <List>
+          {chatItems.map((el, idx) => {
+            return (
+              <Link to="/chat?id={el.id}" style={{ textDecoration: "none" }}>
+                <ListItem>
+                  <ListItemText primary={el.name} key={idx} />
+                </ListItem>
+              </Link>
+            );
+          })}
+        </List>
+      </Drawer>
+    </Box>
+  );
+}
--- src/layouts/dashboard/index.tsx
+++ src/layouts/dashboard/index.tsx
@@ -3,13 +3,14 @@ import { Stack } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import SideBar from "../../components/Shared/Navigation/SideBar";
 import useResponsive from "../../hooks/useResponsive";
+import MobileNav from "../../components/Shared/Navigation/MobileNav";
 
 const DashboardLayout: React.FC = () => {
   const isMobile = useResponsive("between", "md", "xs", "sm");
   return (
     <>
       <Stack direction="row">
-        {!isMobile && <SideBar />}
+        {isMobile ? <MobileNav /> : <SideBar />}
         <Outlet />
       </Stack>
     </>
